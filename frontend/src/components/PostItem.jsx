1dbd8b07b08412f9f159c5c04aaf6ca262240c9c
emirsahovic <emir.sahovic00@gmail.com>
2022-03-11 19:58:46 +0100

    Read more option

--- frontend/src/components/PostItem.jsx
+++ frontend/src/components/PostItem.jsx
@@ -2,15 +2,16 @@ import Moment from 'react-moment';
 import { FaWindowClose } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { deletePost } from '../features/post/postSlice';
+import { Link } from 'react-router-dom';
 
 const PostItem = ({ post }) => {
     const { user } = useSelector(state => state.auth);
     const dispatch = useDispatch();
 
     return (
-        <div className="shadow-xl rounded-lg py-6 px-10 mt-16 bg-gray-200 h-full relative">
+        <div className="shadow-xl rounded-lg py-6 px-10 mt-24 bg-gray-200 h-full relative">
             <h1 className="text-2xl font-bold text-center uppercase">{post.title}</h1>
-            <img src={post.imageUrl} alt="Post image" className='my-3 w-full' />
+            <img src={post.imageUrl} alt="Post image" className='my-3 w-full h-96' />
             <p className="px-4 my-6 text-gray-800 text-lg">{post.text}</p>
             <p className="text-sky-600 absolute bottom-3 left-4">Author: <span className='text-gray-600'>{post.name}</span></p>
             <p className='text-sky-600 absolute bottom-3 right-4'>Published on: <Moment className='text-gray-600' format='DD/MM/YYYY'>{post.createdAt}</Moment></p>
@@ -18,6 +19,7 @@ const PostItem = ({ post }) => {
                 <button onClick={() => dispatch(deletePost(post._id))} className='text-red-500 absolute top-4 right-3 text-2xl hover:opacity-70'>
                     <FaWindowClose />
                 </button>}
+            <Link to={`/post/${post._id}`} className='absolute bottom-24 right-12 bg-sky-500 font-bold text-white px-3 py-2 rounded'>Read More</Link>
         </div>
     )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { getPosts } from "../features/post/postSlice";
 import Spinner from "../components/Spinner";
 import PostItem from "../components/PostItem";
@@ -16,13 +17,25 @@ const Dashboard = () => {
         return <Spinner />
     }
 
-    return (
-        <div className="grid px-28 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 mx-auto container justify-center items-center gap-6">
-            {posts.map(post => (
-                <PostItem key={post._id} post={post} />
-            ))}
-        </div>
-    )
+    if (posts.length > 0) {
+        return (
+            <div className="grid px-28 pb-24 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 mx-auto container justify-center items-center gap-6">
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
+        )
+    } else {
+        return (
+            <div className="mx-auto text-center mt-24">
+                <h1 className="text-3xl">There are no posts yet...</h1>
+                <div className="flex space-x-4 text-center justify-center items-center mt-3">
+                    <h1 className="text-3xl">Be the first to add a post?</h1>
+                    <Link to='/create-post' className="bg-sky-500 font-bold px-3 py-2 text-white rounded hover:opacity-80">Add new post</Link>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Dashboard;
